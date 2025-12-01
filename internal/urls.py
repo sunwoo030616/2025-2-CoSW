c3d6e084b815dfa6734c8e2ddf17b4283d536468
lo2q <suhyeonkvvon@gmail.com>
2025-12-01 18:42:43 +0900

    📝 Update: requirements.txt 최신화

--- internal/urls.py
+++ internal/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import EmailSendView, FoundItemSyncView, DailyMatchingView
-
+from .views import*
 urlpatterns = [
     path("email/send", EmailSendView.as_view(), name="email_send"),
     path("found-items/sync", FoundItemSyncView.as_view(), name="found_items_sync"),
